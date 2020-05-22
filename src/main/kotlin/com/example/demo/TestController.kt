47776813eb5d4d27f4b274011b5ad390102e3e7b
Jarkko Palokangas <jarkko.palokangas@solita.fi>
2020-05-22 10:36:11 +0300

    Initial files

---
+++ src/main/kotlin/com/example/demo/TestController.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController{
+    @GetMapping("/")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            "Hello, $name"
+}
